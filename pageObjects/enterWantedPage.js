278625f55aa247f328246e016cf1e029052b147a
unknown <christinaolson10@gmail.com>
2019-01-31 20:10:38 -0700

    initial folder commit

---
+++ pageObjects/enterWantedPage.js
@@ -0,0 +1,24 @@
+module.exports = {
+    url: 'http://localhost:3000',
+    elements: {
+       header:  'input[name="hdrInput"]',
+       mke: 'input[name="mkeInput"]',
+       oai: 'input[name="oriInput"]',
+       name: 'input[name="namInput"]',
+       sex: 'select[name="sexInput"]',
+       race: 'select[name="racInput"]',
+       height: 'input[name="hgtInput"]',
+       weight: 'input[name="wgtInput"]',
+       hair: 'input[name="haiInput"]',
+       offense: 'input[name="offInput"]',
+       dow: 'input[name="dowInput"]',
+       //optional fields
+       dl: 'input[name="olnInput"]',
+       dlState: 'input[name="olsInput"]',
+       dlExpDate: 'input[name="oldInput"]',
+       lp: 'input[name="licInput"]',
+       lpState: 'input[name="lisInput"]',
+       lpExpDate: 'input[name="lidInput"]',
+
+    },
+}
